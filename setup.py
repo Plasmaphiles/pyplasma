bee5abee3edb6d5f187e0721b51bb24d873951c6
Zachary Westerman <westerman.zachary@gmail.com>
2024-04-21 19:57:39 -0500

    Update version info

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyplasma',
-    version='0.1.0',
-    description='A Python library for Plasma interaction',
-    author='Zachary Westerman',
-    author_email='westerman.zachary@gmail.com',
-    packages=find_packages(),
-    python_requires='>=3.10',
-    install_requires=[
+  name='pyplasma',
+  version='0.2.0',
+  description='A Python library for Plasma interaction',
+  author='Zachary Westerman',
+  author_email='westerman.zachary@gmail.com',
+  packages=find_packages(),
+  python_requires='>=3.10',
+  install_requires=[
 		#Only dependencies are in the stdlib
 	],
 )
